34d10cf5c8a2d1bb4de99c50912a0f898890089d
TimeConqueror <timeconqueror999@gmail.com>
2021-07-04 02:18:47 +0300

    Release 1.16.5? Attempt 3. Fixed WG and disabled some features temporally

--- src/main/java/ru/timeconqueror/lootgames/common/config/ConfigGeneral.java
+++ src/main/java/ru/timeconqueror/lootgames/common/config/ConfigGeneral.java
@@ -1,18 +1,12 @@
 package ru.timeconqueror.lootgames.common.config;
 
-import com.google.common.collect.Lists;
-import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.config.ModConfig;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import ru.timeconqueror.lootgames.LootGames;
 import ru.timeconqueror.timecore.api.common.config.Config;
 import ru.timeconqueror.timecore.api.common.config.ConfigSection;
 import ru.timeconqueror.timecore.api.common.config.IQuickConfigValue;
 import ru.timeconqueror.timecore.api.common.config.ImprovedConfigBuilder;
-
-import java.util.HashMap;
-import java.util.List;
 
 public class ConfigGeneral extends Config {
 
@@ -30,8 +24,8 @@ public class ConfigGeneral extends Config {
                         .define("disable_minigames", false)
         );
 
-//        worldGen = new WorldGenCategory();  //TODO
-//        builder.addAndSetupSection(worldGen);
+        worldGen = new WorldGenCategory();
+        builder.addAndSetupSection(worldGen);
     }
 
     @Override
@@ -42,8 +36,8 @@ public class ConfigGeneral extends Config {
     public static class WorldGenCategory extends ConfigSection {
         public IQuickConfigValue<Boolean> disableDungeonGen;
         public IQuickConfigValue<Integer> maxEntryPathLength;
-        private ForgeConfigSpec.ConfigValue<List<? extends String>> dimAndRhombList;
-        private HashMap<Integer, Integer> dimRhombs;
+//        private ForgeConfigSpec.ConfigValue<List<? extends String>> dimAndRhombList;
+//        private HashMap<Integer, Integer> dimRhombs;//TODO
 
         public WorldGenCategory() {
             super("worldgen", "Regulates dungeon appearing in world.");
@@ -56,12 +50,12 @@ public class ConfigGeneral extends Config {
                             .define("disable_dungeon_gen", false)
             );
 
-            dimAndRhombList = builder.comment("Whitelisted dimensions' ids that were allowed for dungeon generation and rhomb size.",
-                    "Rhomb size means the size of rhombs, which will imaginary cover the world. Dungeon will be generated in each rhomb.",
-                    "So the larger the size, the less chance of generation.",
-                    "Rhomb size must be between 5 and 100.",
-                    "Example of array element: 0; 20 - this means that dungeons will be generated in rhombs with size equal to 20 in the overworld (ID = 0).")
-                    .defineList("dim_and_rhomb_list", Lists.newArrayList("0; 20"), o -> true);//TODO someday nightconfig will fix validators...
+//            dimAndRhombList = builder.comment("Whitelisted dimensions' ids that were allowed for dungeon generation and rhomb size.",
+//                    "Rhomb size means the size of rhombs, which will imaginary cover the world. Dungeon will be generated in each rhomb.",
+//                    "So the larger the size, the less chance of generation.",
+//                    "Rhomb size must be between 5 and 100.",
+//                    "Example of array element: 0; 20 - this means that dungeons will be generated in rhombs with size equal to 20 in the overworld (ID = 0).")
+//                    .defineList("dim_and_rhomb_list", Lists.newArrayList("0; 20"), o -> true);//TODO someday nightconfig will fix validators...
 
             maxEntryPathLength = builder.optimized(
                     builder.comment("Defines the maximum length of the entry path from ground to the structure.",
@@ -71,52 +65,52 @@ public class ConfigGeneral extends Config {
             );
         }
 
-        public boolean isDimensionEnabledForWG(int worldID) {
-            return dimRhombs.get(worldID) != null;
-        }
-
-        public int getRhombSizeFromDim(int dimensionID) {
-            return dimRhombs.get(dimensionID);
-        }
+//        public boolean isDimensionEnabledForWG(int worldID) {
+//            return dimRhombs.get(worldID) != null;
+//        }
+//
+//        public int getRhombSizeFromDim(int dimensionID) {
+//            return dimRhombs.get(dimensionID);
+//        }
 
         @Override
         public void onEveryLoad(ModConfig.ModConfigEvent configEvent) {
 
-            parseDimAndRhombList();
+//            parseDimAndRhombList();
         }
 
-        private void parseDimAndRhombList() {
-            dimRhombs = new HashMap<>();
-
-            List<? extends String> dimAndRhombList = this.dimAndRhombList.get();
-
-            for (String entry : dimAndRhombList) {
-                if (entry.length() == 0)
-                    return;
-
-                String[] arr = entry.split(";");
-                if (arr.length != 2) {
-                    LootGames.LOGGER.error("Invalid dimension rhomb entry found: {}. Syntax is <dimensionID>;<rhomb size>. This entry will be skipped.", entry);
-                }
-
-                try {
-                    int dimID = Integer.parseInt(arr[0].trim());
-                    int rhombSize = Integer.parseInt(arr[1].trim());
-
-                    if (rhombSize < 5 || rhombSize > 100) {
-                        LootGames.LOGGER.error("Invalid dimension rhomb entry found: {}. Rhomb size must be between 5 and 100.", entry);
-                    } else {
-                        if (!dimRhombs.containsKey(dimID)) {
-                            dimRhombs.put(dimID, rhombSize);
-                            LootGames.LOGGER.info("Worldgen enabled in dimension {} with rhomb size {}.", dimID, rhombSize);
-                        } else
-                            LootGames.LOGGER.error("Invalid dimension rhomb entry found: {}. DimensionID is already defined.", entry);
-                    }
-
-                } catch (NumberFormatException e) {
-                    LootGames.LOGGER.error("Invalid dimension rhomb entry found: {}. DimensionID or Rhomb size is not an Integer. This entry will be skipped.", entry);
-                }
-            }
-        }
+//        private void parseDimAndRhombList() {
+//            dimRhombs = new HashMap<>();
+//
+//            List<? extends String> dimAndRhombList = this.dimAndRhombList.get();
+//
+//            for (String entry : dimAndRhombList) {
+//                if (entry.length() == 0)
+//                    return;
+//
+//                String[] arr = entry.split(";");
+//                if (arr.length != 2) {
+//                    LootGames.LOGGER.error("Invalid dimension rhomb entry found: {}. Syntax is <dimensionID>;<rhomb size>. This entry will be skipped.", entry);
+//                }
+//
+//                try {
+//                    int dimID = Integer.parseInt(arr[0].trim());
+//                    int rhombSize = Integer.parseInt(arr[1].trim());
+//
+//                    if (rhombSize < 5 || rhombSize > 100) {
+//                        LootGames.LOGGER.error("Invalid dimension rhomb entry found: {}. Rhomb size must be between 5 and 100.", entry);
+//                    } else {
+//                        if (!dimRhombs.containsKey(dimID)) {
+//                            dimRhombs.put(dimID, rhombSize);
+//                            LootGames.LOGGER.info("Worldgen enabled in dimension {} with rhomb size {}.", dimID, rhombSize);
+//                        } else
+//                            LootGames.LOGGER.error("Invalid dimension rhomb entry found: {}. DimensionID is already defined.", entry);
+//                    }
+//
+//                } catch (NumberFormatException e) {
+//                    LootGames.LOGGER.error("Invalid dimension rhomb entry found: {}. DimensionID or Rhomb size is not an Integer. This entry will be skipped.", entry);
+//                }
+//            }
+//        }
     }
 }
--- src/main/java/ru/timeconqueror/lootgames/registry/LGStructures.java
+++ src/main/java/ru/timeconqueror/lootgames/registry/LGStructures.java
@@ -2,13 +2,12 @@ package ru.timeconqueror.lootgames.registry;
 
 import net.minecraft.world.gen.feature.NoFeatureConfig;
 import ru.timeconqueror.lootgames.LootGames;
+import ru.timeconqueror.lootgames.common.config.LGConfigs;
 import ru.timeconqueror.lootgames.common.world.gen.GameDungeonStructure;
 import ru.timeconqueror.timecore.api.registry.StructureRegister;
 import ru.timeconqueror.timecore.api.registry.StructureRegister.StructureHolder;
 import ru.timeconqueror.timecore.api.registry.util.AutoRegistrable;
 
-////TODO move to TimeCore registry system
-//@TimeAutoRegistrable(target = TimeAutoRegistrable.Target.CLASS)
 public class LGStructures {
     @AutoRegistrable
     private static final StructureRegister REGISTER = new StructureRegister(LootGames.MODID);
@@ -16,48 +15,10 @@ public class LGStructures {
     public static StructureHolder<NoFeatureConfig, GameDungeonStructure> GAME_DUNGEON =
             REGISTER.register("game_dungeon",
                     GameDungeonStructure::new,
-                    StructureRegister.TimeStructureSeparationSettings.create(10, 5),
+                    StructureRegister.TimeStructureSeparationSettings.create(10, 6),
                     NoFeatureConfig.CODEC,
                     NoFeatureConfig.INSTANCE
             )
+                    .setDimensionPredicate(serverWorld -> !LGConfigs.GENERAL.worldGen.disableDungeonGen.get())
                     .asHolder();
-
-//    public static final GameDungeonStructure GAME_DUNGEON = new GameDungeonStructure(NoFeatureConfig::deserialize);
-//
-//    public static IStructurePieceType GD_PIECE_MAIN_ROOM;
-//
-//    /**
-//     * Register features
-//     * Structures are a type of feature so they get registered here too
-//     */
-//    @SubscribeEvent
-//    public static void registerFeatures(RegistryEvent.Register<Feature<?>> event) {
-//        //Using the registry directly like this is bad, however this is currently the only way to register a structure piece
-//        //Specifically, this is to avoid the error that occurs in StructurePiece.write()
-//        //The piece's registry id is saved to nbt, and without the structure will not be saved properly and users will experience errors in the console whenever the chunk is loaded or unloaded
-//        GD_PIECE_MAIN_ROOM = Registry.register(Registry.STRUCTURE_PIECE, LootGames.MODID + ":game_dungeon/main_room", GameDungeonPieces.MainRoom::new);
-//
-//        GAME_DUNGEON.setRegistryName(LootGames.MODID, "game_dungeon");
-//        event.getRegistry().register(GAME_DUNGEON);
-//    }
-//
-//    /**
-//     * Add structures and features to biomes
-//     * Should be called after the registry events have run, so that all biomes and features already exist
-//     * Using categories like this is just one way of choosing which biome to place a feature in
-//     * It is important that each structure be added both as a feature and as a structure
-//     */
-//    @SubscribeEvent
-//    public static void applyFeatures(FMLCommonSetupEvent event) {
-//        for (Biome biome : ForgeRegistries.BIOMES.getValues()) {
-//            biome.addStructure(GAME_DUNGEON, IFeatureConfig.NO_FEATURE_CONFIG);
-//            /*
-//             * Structures should be added as features to any biome that they might overlap into, to avoid generation being cut off at chunk edges
-//             * Structures can be added as structures to any biome they should start in
-//             *
-//             * VEGETAL_DECORATION is the last stage
-//             */
-//            biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(GAME_DUNGEON, IFeatureConfig.NO_FEATURE_CONFIG, Placement.NOPE, IPlacementConfig.NO_PLACEMENT_CONFIG));
-//        }
-//    }
 }
